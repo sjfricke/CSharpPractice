c31fd32eadf3d946f19da03f992ac3e969f38865
sjfricke <sjfricke@wisc.edu>
2015-07-05 17:23:28 -0500

    C# files

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+public class SortAlphabetLength : IComparer
+{
+    public int Compare(Object x, Object y)
+    {
+        if (x.ToString().Length == y.ToString().Length)
+            return string.Compare(x.ToString(), y.ToString());
+        else if (x.ToString().Length > y.ToString().Length)
+            return -1;
+        else
+            return 1;
+    }
+}
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+
+       
+        StreamReader reader = File.OpenText(args[0]);
+        string first = reader.ReadLine();
+        int num = Convert.ToInt32(first);
+        ArrayList strings = new ArrayList();
+        while (!reader.EndOfStream)
+        {
+            strings.Add(reader.ReadLine());
+          
+        }
+
+        SortAlphabetLength alphaLen = new SortAlphabetLength();
+        strings.Sort(alphaLen);
+
+        for (int i = 0; i < num; i++)
+        {
+            Console.WriteLine(strings[i]);
+        }
+        //Console.ReadLine();
+    }
+}
